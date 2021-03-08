779e818ca8420186b36fb7d688fee0e5ff9f5d8c
BEN YOUSSEF, Elyes <elyes.by@gmail.com>
2021-03-08 14:42:19 -0500

    add logger for test

--- AzChallangeCalicotApi/Controllers/CollectionController.cs
+++ AzChallangeCalicotApi/Controllers/CollectionController.cs
@@ -2,7 +2,10 @@
 using AzChallangeCalicotApi.Type.Interfaces;
 using AzChallangeCalicotApi.Type.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace AzChallangeCalicotApi.Controllers
 {
@@ -11,15 +14,26 @@ namespace AzChallangeCalicotApi.Controllers
     public class CollectionController : ControllerBase
     {
         private IProductService _productService;
+        private IConfiguration _configuration;
 
-        public CollectionController(IProductService productService)
+        public CollectionController(IProductService productService, IConfiguration configuration)
         {
             _productService = productService;
+            _configuration = configuration;
         }
         [HttpGet("all")]
         public ActionResult<EnveloppeReponse<List<Produit>>> GetAllProducts()
         {
-            return this.OkAvecEnveloppe(_productService.GetProducts());
+            try
+            {
+                return this.OkAvecEnveloppe(_productService.GetProducts());
+            }
+            catch (Exception ex)
+            {
+                var msg = $"GetToken={_configuration.GetValue<bool>("GetToken")} UserAssignedIdentity={_configuration.GetValue<string>("UserAssignedIdentity")} DefaultConnection={_configuration.GetConnectionString("DefaultConnection")}";
+                throw new Exception(msg, ex);
+            }
+            
         }
 
         [HttpPost]
